f015173398746e266293fb97f61d8dd4c8b86cc7
woobin <guiyomyee@gmail.com>
2026-03-17 16:50:18 +0900

    week03 넘íãv0.1

--- .venv/ncs_kiosk.py
+++ .venv/ncs_kiosk.py
@@ -1,6 +1,29 @@
 import statistics
+
+from pandas.core.dtypes.common import pandas_dtype
+
 data = [10,20,30,40]
 average = statistics.mean(data)
 print (average)
 
+
+# 넘파이 활용
+import numpy as np
+data = [10,20,30,40]
+average = np.mean(data)
+print(average)
+
+
+# 판다스 사용
+import pandas as pd
+data = [10,20,30,40]
+average = pd.mean(data)
+print(average)
+
+
 # 코드 최적화에 대한 수업을 하시는 것같음
+#
+#
+#
+#
+#
